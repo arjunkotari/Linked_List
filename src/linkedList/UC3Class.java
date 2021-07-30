73ffec9136f7f2efb65c7d89abb4f714f7bb6572
arjunkotari <arjun.kotari123@gmail.com>
2021-07-30 17:02:47 +0530

    Added:Created Linked List by using Append method

---
+++ src/linkedList/UC3Class.java
@@ -0,0 +1,15 @@
+package linkedList;
+
+public class UC3Class {
+	public static void main(String[] args) {
+		INode myFirstNode = new MyNode<Integer>(70);
+		INode mySecondNode = new MyNode<Integer>(30);
+		INode myThirdNode = new MyNode<Integer>(56);
+		
+		MyLinkedList myLinkList = new MyLinkedList();
+		myLinkList.append(myFirstNode);
+		myLinkList.append(mySecondNode);
+		myLinkList.append(myThirdNode);
+		myLinkList.printNode();
+	}
+}
